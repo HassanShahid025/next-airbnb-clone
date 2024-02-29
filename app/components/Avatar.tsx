855be4afb8c61524bcc0a6aa929a38736868e6a3
hassan shahid <hassanshahid234@gmail.com>
2024-02-29 19:38:06 +0500

    navbar ui completed

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Image from "next/image";
+
+type AvatarProps = {
+    src: string | null | undefined;
+}
+const Avatar = () => {
+  return (
+    <Image 
+    className="rounded-full" 
+    height="30" 
+    width="30" 
+    alt="Avatar" 
+    src={'/images/placeholder.jpg'}
+  />
+  )
+}
+
+export default Avatar
