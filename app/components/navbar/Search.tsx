18040e28985fb6af79cf0719643ebe04078c468a
hassan shahid <hassanshahid234@gmail.com>
2024-03-28 01:22:38 +0500

    middleware added

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -9,6 +9,48 @@ import useCountries from '@/app/hooks/useCountries';
 
 const Search = () => {
   const searchModal = useSearchModal()
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const  locationValue = params?.get('locationValue'); 
+  const  startDate = params?.get('startDate');
+  const  endDate = params?.get('endDate');
+  const  guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return 'Anywhere';
+  }, [locationValue, getByValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
+
+
+
   return (
     <div
       onClick={searchModal.open}
@@ -39,7 +81,7 @@ const Search = () => {
           px-6
         "
         >
-          Anywhere
+          {locationLabel}
         </div>
         <div
           className="
@@ -53,7 +95,7 @@ const Search = () => {
           text-center
         "
         >
-          Any Week
+          {durationLabel}
         </div>
         <div
           className="
@@ -67,7 +109,7 @@ const Search = () => {
           gap-3
         "
         >
-          <div className="hidden sm:block">Add Guest
+          <div className="hidden sm:block">{guestLabel}
         </div>
           <div
             className="
